a545b3392aafeca3f6f6cae0fef2a70e559cf6df
lalit-collab <lalitkumar.insights@gmail.com>
2025-06-21 19:49:30 +0530

    commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
 
-const connectDB = async () =>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected")
-        )
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickblog`)
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log("MongoDB connection error:", error.message);
+  }
+};
 
 export default connectDB;
